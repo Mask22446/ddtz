505f7c769f93e79fc6e9b5faa923e7f7933596a4
Maksim Vlasov <54456828+Mask22446@users.noreply.github.com>
2024-02-14 21:55:14 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+namespace uniqгуWordsCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathToFile = "Grey.txt";
+            Dictionary<string,int> keyValuePairs = new Dictionary<string,int>();
+            foreach(var line in File.ReadLines(pathToFile))
+            {
+                var words = line.Split(new char[] { ' ', ',', '.', '!', '?', ':', '-', '–' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach(var word in words)
+                {
+                    if (!keyValuePairs.ContainsKey(word))
+                    {
+                        keyValuePairs.Add(word, 1);
+                    }
+                    else
+                    {
+                        keyValuePairs[word]++;
+                    }
+                }
+            }
+            using(StreamWriter file =  new StreamWriter("dict.txt")) {
+                foreach(var row in keyValuePairs.OrderByDescending(_=>_.Value)) { 
+                    file.WriteLine($"{row.Key} {row.Value}");
+                }
+            }
+            Console.WriteLine("Файл создан");
+        }
+    }
+}
